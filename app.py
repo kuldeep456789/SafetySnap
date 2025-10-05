415206fba0fbf85cf99d0ea15935292367c0c569
kuldeep023 <kuldeeppraj2002@gmail.com>
2025-10-05 07:19:27 +0530

    chanegs in backend file

--- app.py
+++ app.py
@@ -140,4 +140,5 @@ def export_pdf():
 # Run Flask App
 # -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
